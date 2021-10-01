0c3285e08851a8b35d11cf6070b93b1654bb7ece
PolinaYu <57799907+PolinaYu@users.noreply.github.com>
2021-10-01 22:29:57 +0300

    fix: fix case

--- src/what-season.js
+++ src/what-season.js
@@ -22,16 +22,24 @@ export default function getSeason( date ) {
   }
   let season;
   switch(date.getMonth()){
-    case (11||0||1): 
+    case 11:
+    case 0:
+    case 1: 
       season ='winter';
       break;
-    case (2||3||4):
+    case 2:
+    case 3:
+    case 4:
       season = 'spring';
       break;
-    case (5||6||7):
+    case 5:
+    case 6:
+    case 7:
       season = 'summer';
       break;
-    case (8||9||10):
+    case 8:
+    case 9:
+    case 10:
       season = 'autumn';
       break;
   }
