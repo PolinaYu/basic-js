5815c42667b370a1f5685a98ace39e791e5a7def
PolinaYu <57799907+PolinaYu@users.noreply.github.com>
2021-10-03 23:37:55 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,30 +6,39 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default {
   chain: [],
+
   getLength() {
-  return this.chain.length; 
+    return this.chain.length;
   },
-  addLink( value ) {
-    if(String(value)){
-      this.chain.push('(' + (String(value)) + ')');
-      return this; 
-      }
-    },
-  removeLink(position ) {
-    if( typeof position !== 'number' || position > this.chain.length || position < 1){
-      this.chain = [];
-      throw new Error("You can't remove incorrect link!");
-    }  
-    this.chain.splice(position-1, 1);
+
+  addLink(value) {
+    if (typeof(value) === "boolean" || value === null || isNaN(value) || value === 0){
+      value = String(value);
+    }
+    if (!value){
+      this.chain.push('( )');
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
     return this;
   },
-  reverseChain() {
-   this.chain.reverse();
-   return this;
+  removeLink(position) {
+    if(isNaN(position) || !Number.isInteger(position) || position <= 0 || position >= this.getLength()) {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }  
+    this.chain.splice(position - 1, 1);
+    return this;    
   },
+
+  reverseChain() {
+    this.chain.reverse();
+    return this;
+  },
+
   finishChain() {
     let result = this.chain.join('~~');
-    this.chains = [];
+    this.chain = [];
     return result;
   }
 };
