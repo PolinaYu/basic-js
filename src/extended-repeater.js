9fe988d3c41aa3080a15ea6a0fa44dd852dbc133
PolinaYu <57799907+PolinaYu@users.noreply.github.com>
2021-10-03 21:43:45 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,23 +16,31 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function repeater(str, options ) {
-  let{
-    repeatTimes = 1,
-    separator = '+',
-    addition = '',
-    additionRepeatTimes = 1,
-    additionSeparator ='|'
-  } = options
-  let result = String(str);
-  let additional ='';
-  for(let j = 0; j < additionRepeatTimes; j++){
-    if(j == 0) additional += addition;
-    else additional += additionSeparator + addition;
+  str = String(str);
+  let additional = "";
+  let newStr = "";
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
   }
-  for(let i = 0; i < repeatTimes; i++){
-    if(i == 0) result = str;
-    else result += separator + str;
-  
+  if (!options.separator) {
+    options.separator = "+";
   }
-  return result;
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+  for (let i = 1; i < options.additionRepeatTimes; i++) {
+    additional += options.addition + options.additionSeparator;
+  }
+  additional += options.addition;
+  for (let j = 1; j < options.repeatTimes; j++) {
+    newStr += str + additional + options.separator;
+  }
+  newStr += str + additional;
+  return newStr;
 }
