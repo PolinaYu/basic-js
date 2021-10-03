7e14a52af7c5d79f1ef06c5d000b6ba0063a33ee
PolinaYu <57799907+PolinaYu@users.noreply.github.com>
2021-10-03 22:41:10 +0300

    Update st-mine-sweeper.js

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -24,21 +24,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]
  */
 export default function minesweeper ( matrix ) {
-  let result;
-  let count = 0;
-  for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix[i].length; j++){
-      if(typeof matrix[i-1][j] !== 'undefined'&& matrix[i-1][j])count++
-      if(typeof matrix[i][j-1] !== 'undefined' && matrix[i][j-1])count++
-      if(typeof matrix[i+1][j] !== 'undefined' && matrix[i+1][j]) count++
-      if(typeof matrix[i][j+1] !== 'undefined' && matrix[i][j+1]) count++
-      if(typeof matrix[i+1][j-1] !== 'undefined' && matrix[i+1][j-1]) count++
-      if(typeof matrix[i+1][j+1] !== 'undefined' && matrix[i+1][j+1]) count++
-      if(typeof matrix[i-1][j+1] !== 'undefined' && matrix[i-1][j+1]) count++
-      if(typeof matrix[i-1][j-1] !== 'undefined' && matrix[i-1][j-1]) count++
-      result[i][j] = count;
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = matrix[i].slice();
+  }
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      result[i][j] = 0;
+      if (typeof matrix[i - 1] !== 'undefined') {
+        if (matrix[i - 1][j - 1] === true) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j] === true) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j + 1] === true) {
+          result[i][j]++;
+        }
+      }
+      if (matrix[i][j - 1] === true) {
+        result[i][j]++;
+      }
+      if (matrix[i][j + 1] === true) {
+        result[i][j]++;
+      }
+      if (typeof matrix[i + 1] !== 'undefined') {
+        if (matrix[i + 1][j - 1] === true) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j] === true) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j + 1] === true) {
+          result[i][j]++;
+        }
+      }
     }
   }
-
   return result;
 }
